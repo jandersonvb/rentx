a51542f5075e04d13c8e02c3cc2a5e415b697788
jandersonp <jandersonpizziallyvilasboas@gmail.com>
2022-05-05 21:57:46 -0300

    chore: config ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running"));
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => console.log('Server is running'));
